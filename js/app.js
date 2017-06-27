a746fb9f3b001e46661ff80b88ff18869b0c98b4
Gil Roman <gil@digitalgil.com>
2017-06-27 11:03:13 -0700

    refactored code and eliminated search button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Data for the Silverlake Neighborhood Explorer
 var places = [{
 		name:'Night + Market Song',
@@ -5,7 +7,8 @@ var places = [{
 		tags:['Thai', 'Restaurant'],
 		streetAddress:'3322 W. Sunset Blvd.',
 		city:'Los Angeles',
-		state:'CA'
+		state:'CA',
+		latLong: {lat: 34.086907, lng: -118.275844 }
 	},
 	{
 		name:'Still Yoga',
@@ -13,78 +16,117 @@ var places = [{
 		tags:['Yoga','Studio'],
 		streetAddress:'2395 Glendale Blvd',
 		city:'Los Angeles',
-		state:'CA'
+		state:'CA',
+		latLong: {lat: 34.099983, lng: -118.259545}
 	}];
 
-var neighborhood = 'Silverlake';
-
+// Global variable referencing to the google map object
 var map;
 
+// Array for markers.
+var markers = [];
 
 // Model - A Class to represent a place listed in the neighborhood map
-function Place(data){
-	var self = this;
-	self.name = ko.observable(data.name);
-	self.typeOfBusiness = ko.observable(data.typeOfBusiness);
-	self.tags = ko.observableArray(data.tags);
-	self.streetAddress = ko.observable(data.streetAddress);
-	self.city = ko.observable(data.city);
-	self.state = ko.observable(data.state);
-};
-
+var Place = function(data) {
+	this.name = ko.observable(data.name);
+	this.typeOfBusiness = ko.observable(data.typeOfBusiness);
+	this.tags = ko.observableArray(data.tags);
+	this.streetAddress = ko.observable(data.streetAddress);
+	this.city = ko.observable(data.city);
+	this.state = ko.observable(data.state);
+	this.latLong = ko.observable(data.latLong);
+}
 
 // ViewModel
-function locationsViewModel(){
+function locationsViewModel(places){
+	var self = this;
 
-var self = this;
+	// An observable array to store locations data
+	self.listings = ko.observableArray(places);
 
-// An empty array to store location data
-self.listings = ko.observableArray([]);
+	// A variable for the search input box characters
+	self.query = ko.observable('');
 
-// A variable for the query term.
-self.query = ko.observable();
-
-self.filteredList = ko.computed(function(){});
-
-// Store the data for each place in the observable array
-places.forEach(function(listing){
-	self.listings.push(new Place(listing));
-});
-
-// Filtered List   STILL NEED TO MAKE THE TAGS ARRAY ALL TO LOWER CASE FOR COMPARISON WITH THE QUERY
-self.filteredList = ko.computed(function(query){
-	if(!self.query()){ return self.listings();}
-	else {
-		var filtered = ko.utils.arrayFilter(self.listings(), function(listing){
-		return (listing.name().toLowerCase().indexOf(self.query().toLowerCase())>-1 || listing.tags().indexOf(self.query())>-1);
+	// Filter the Listings by name or tags
+	self.filteredList = ko.computed(function(query){
+		var filteredArray = [];
+		console.log(self.query());
+		if(self.query()==''){
+			filteredArray = self.listings();
+		} else {
+			filteredArray = ko.utils.arrayFilter(self.listings(), function(listing){
+				return (listing.name.toLowerCase().indexOf(self.query().toLowerCase())>-1 ||
+				listing.tags.join(' ').toLowerCase().indexOf(self.query().toLowerCase())>-1);
+			});
+		}
+		return filteredArray;
 	});
-		return filtered;
-	}
-});
 
-// Filter the list of the observable array listings by the query entered on the serach input box
-self.filterList = function(){
-	return self.filteredList();
+	// Function that shows a marker if it's in the filtered list and hides it if it's not
+	// It compares the marker's title to the listing's name
+	self.showMarkers = ko.computed(function(){
+		var filteredArray = self.filteredList();
+		for(var m=0; m<markers.length; m++){
+			for(var l=0; l<filteredArray.length; l++){
+				if (markers[m].title==filteredArray[l].name){
+					markers[m].setVisible(true);
+					break;
+				} else {
+					markers[m].setVisible(false);
+				}
+			}
+		}
+	});
+
+	// Variable for the last clicked on marker on the list
+	self.currentMarker = null;
+
+	//Function that makes a marker for every listing in the filtered list
+	//pushes a marker to the markers array, creates and infoWindow on the marker
+	self.markersOnList = function(){
+			self.clearMarkers();
+			markers = [];
+
+			for (var i=0; i<self.filteredList().length; i++){
+				var infowindow = null;
+
+				markers.push(drawMarker(self.filteredList()[i]));
+				infowindow = markerInfoWindow(self.filteredList()[i]);
+				bindInfoWindow(markers[i], infowindow, map);
+			}
+	};
+
+	// Function to set clear all markers in the map by setting to to null the map attribute of each marker
+	self.clearMarkers = function() {
+		for(var i = 0; i<markers.length; i++){
+			markers[i].setMap(null);
+		}
+	};
+
+	// Function that used the index in the array of the listing clicked and triggers the corresponding marker
+	self.listingClicked = function(index) {
+		google.maps.event.trigger(markers[index], 'click');
+		self.currentMarker = markers[index];
+	};
+
+	// Placeholder for the search function
+	self.search = function(){
+
+	};
+
+	// Check to see if Google Map has loaded, if not display a message indicating it's waiting to connect
+	if (typeof google === 'undefined') {
+		var mapError = document.getElementById('map');
+		mapError.innerHTML = '<p class="errorMessage">Waiting to connect to Google Map API...</p>';
+	} else {
+			for (var i=0; i<self.listings().length; i++){
+				markers.push(self.drawMarker(self.listings()[i]));
+
+			}
+		}
 }
 
-//Check to see if google api has loaded
-if (typeof google === 'undefined') {
-	var mapError = document.getElementById('map');
-	mapError.innerHTML = '<p class="errorMessage">Having trouble connecting to Google Map API...</p>';
-} else {
-// Initialize Google Map
-initMap();
-
-self.filteredList().forEach(function(listing){
-	setMarker(listing.streetAddress());
-});
-
-}
-
-}
-
-ko.applyBindings(new locationsViewModel());
-
+ko.applyBindings(new locationsViewModel(places));
 
 /*****************
 *								 *
@@ -92,33 +134,54 @@ ko.applyBindings(new locationsViewModel());
 *								 *
 *****************/
 
-
-function initMap() {
+//Initializes the map and draws the markers
+var initMap = function(){
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 34.0869409, lng: -118.2702036},
     zoom: 14
   });
+
+  //Draws the markers when the map initializes
+  for (var i=0; i<places.length; i++){
+  	var marker = drawMarker(places[i]);
+  	markers.push(marker);
+  	var infowindow = markerInfoWindow(places[i]);
+  	bindInfoWindow(marker, infowindow, map);
+  }
+};
+
+// Function that takes a listing object as a parameter and draws a single marker
+var drawMarker = function(listing){
+	var marker = new google.maps.Marker({
+		map: map,
+		position: listing.latLong,
+		title: listing.name,
+		animation: google.maps.Animation.DROP
+	});
+	return marker;
+};
+
+// Function that creates a new infowindow on a marker with listing information
+var markerInfoWindow = function(listing){
+	var contentString = '<div class="infoWindow">'+listing.name+'</div>';
+	var infowindow = new google.maps.InfoWindow({
+  	content: contentString
+  });
+	return infowindow;
 }
 
+// Function that makes a marker bounce for the amount of time indicated with the timeout argument (in milliseconds)
+var bounce = function(marker, timeout) {
+	marker.setAnimation(google.maps.Animation.BOUNCE);
+  	window.setTimeout(function(){
+  		marker.setAnimation(null);
+  	}, timeout);
+ }
 
-function setMarker(address) {
-//Initialize the geocoder.
-var geocoder = new google.maps.Geocoder();
-
-//Geocode the location, then create a marker on the map
-geocoder.geocode(
-{
-	address: address,
-	componentRestrictions: {locality: 'Los Angeles'}
-}, function (results, status) {
-	if(status == 'OK') {
-		var marker = new google.maps.Marker({
-			map: map,
-			position: results[0].geometry.location,
-			animation: google.maps.Animation.DROP
-		});
-	} else {
-		window.alert('Google Geocoding was not successful because of: ' +status);
-		}
-});
+// Function that binds a click event listener to a marker which will open an infowindo
+var bindInfoWindow = function(marker, infowindow, map){
+	marker.addListener('click', function(){
+		infowindow.open(map, marker);
+		bounce(marker, 2000);
+	});
 }
