dc9d02d7a053df35026a89cc3ca64c6d985f600f
Gil Roman <gil@digitalgil.com>
2017-07-01 20:55:27 -0700

    Changed comparison on line 62 to use strict comparison '==='

--- public/js/app.js
+++ public/js/app.js
@@ -59,7 +59,7 @@ function LocationsViewModel(places){
 	// It compares the marker's title to the listing's name
 	self.showMarkers = ko.computed(function(){
 		var filteredArray = self.filteredList();
-		if (filteredArray.length == 0){
+		if (filteredArray.length === 0){
 			markers.forEach(function(marker){
 				marker.setVisible(false);
 			});
